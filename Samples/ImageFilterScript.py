6b84fd25a074f257871df38373a36b6d1640c191
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-10-12 01:11:17 -0700

    ImageFilterScript edit with color

--- Samples/ImageFilterScript.py
+++ Samples/ImageFilterScript.py
@@ -10,8 +10,11 @@ path = os.path.join(my_path, "../Rainbow trout-steelhead/*.jpg")
 filelist = glob.glob('**/*.jpg',recursive=True)
 for file in filelist:
 	try:
-		img = cv2.imread(file, 0) # scrolls through all the fish images as of late.
-		cv2.imshow('image',img)
+		img = cv2.imread(file, 1) # scrolls through all the fish images as of late.
+		small = cv2.resize(img, (400,250)) #changes images into a 400px by 250px size.
+		small = cv2.cvtColor(small, cv2.COLOR_BGR2Lab) #possible color change for cvtColor
+		cv2.imshow('image',small)
+
 		cv2.waitKey(0)
 		cv2.destroyAllWindows
 	except IOError as exc:
