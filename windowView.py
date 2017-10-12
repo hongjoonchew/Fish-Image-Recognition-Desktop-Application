f0ac2c6ae1a6e9903f9d7932aebd592a66a0cc49
ColePBryan <cole.bryan.p@gmail.com>
2017-10-11 19:45:30 -0700

    reformatted to match PEP8 guidelines

--- windowView.py
+++ windowView.py
@@ -1,14 +1,16 @@
 import sys
 from PyQt5 import QtWidgets
 
+
 def window():
-	app = QtWidgets.QApplication(sys.argv)
-	w = QtWidgets.QWidget()
-	boxLayout = QtWidgets.QGroupBox()
-	
-	w.setWindowTitle('Fish Recognition Prototype')
-	w.setGeometry(100,100,300,400)
-	w.show()
-	sys.exit(app.exec())
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    box_layout = QtWidgets.QGroupBox()
+
+    w.setWindowTitle('Fish Recognition Prototype')
+    w.setGeometry(100, 100, 300, 400)
+    w.show()
+    sys.exit(app.exec())
+
 
 window()
