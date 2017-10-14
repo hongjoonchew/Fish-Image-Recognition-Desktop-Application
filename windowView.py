cb64db9c3a554c67a67966d9a33a1c8f9c273067
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-10-14 14:15:13 -0700

    Adding a button to the View.

--- windowView.py
+++ windowView.py
@@ -2,15 +2,36 @@ import sys
 from PyQt5 import QtWidgets
 
 
-def window():
-    app = QtWidgets.QApplication(sys.argv)
-    w = QtWidgets.QWidget()
-    box_layout = QtWidgets.QGroupBox()
+class Window(QtWidgets.QWidget):
 
-    w.setWindowTitle('Fish Recognition Prototype')
-    w.setGeometry(100, 100, 300, 400)
-    w.show()
-    sys.exit(app.exec())
+	def __init__(self):
+		super().__init__()
+		self.setGeometry(100,100,300,400)
+		self.setWindowTitle('Fish Recognition Prototype')
+		v_box = QtWidgets.QVBoxLayout()
+		v_box = self.addVBoxLayout(v_box);
+		self.setLayout(v_box)
+		self.show()
+
+	def addVBoxLayout(self, v_box):
+		execute_button = QtWidgets.QPushButton("Execute");
+		v_box.addWidget(execute_button)
+		return v_box
 
 
-window()
+
+		
+
+app = QtWidgets.QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec())
+
+
+
+
+
+
+
+
+
+
