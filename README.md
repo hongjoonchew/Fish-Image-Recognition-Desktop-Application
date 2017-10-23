e748974663065e6aaf3fdff9a2419942fcd31544
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-10-23 10:49:23 -0700

    Cleared some files, added README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Fish-Image-Recognition-Desktop-Application
+#ENGR 195C Engineering Projects in Community Service
+##Underwater ROV project
+###Hung Nguyen, Hong Joon Chew, Mahmood Abuzaina, Anish Puthuparambil, Laith Alquran, Bryan Cole (1 semester), Josh Kardos (1 semester)
+Fish Image Recognition Software (Desktop Application)
+
+GitHub links
+Hung Nguyen (@sagittarius1912)
+Hong Joon Chew (@hongjoonchew)
+Mahmood Abuzaina
+Anish Puthuparambil
+Laith Alquran
+Bryan Cole (@ColePBryan)
+Josh Kardos (@JoshKardos)
--- Samples/ImageFilterScript.py
+++ Samples/ImageFilterScript.py
@@ -8,13 +8,14 @@ from pathlib import Path
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "../Rainbow trout-steelhead/*.jpg")
 filelist = glob.glob('**/*.jpg',recursive=True)
+string = 0;
 for file in filelist:
 	try:
 		img = cv2.imread(file, 1) # scrolls through all the fish images as of late.
 		small = cv2.resize(img, (400,250)) #changes images into a 400px by 250px size.
 		small = cv2.cvtColor(small, cv2.COLOR_BGR2Lab) #possible color change for cvtColor
-		cv2.imshow('image',small)
-
+		cv2.imwrite(string++,small)
+		cv2.imshow('Fish filter img',small)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows
 	except IOError as exc:
