9a104d6ce98a6cb0b6f22a65891f583c03b90f02
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-11-06 17:01:46 -0800

    Filter script lacks save function

--- Samples/ImageFilterScript.py
+++ Samples/ImageFilterScript.py
@@ -4,17 +4,17 @@ import sys
 import os
 import glob
 from pathlib import Path
+from color_transfer import color_transfer
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "../Rainbow trout-steelhead/*.jpg")
 filelist = glob.glob('**/*.jpg',recursive=True)
-string = 0;
+murky_water =  "murky_water.jpg"
 for file in filelist:
 	try:
 		img = cv2.imread(file, 1) # scrolls through all the fish images as of late.
 		small = cv2.resize(img, (400,250)) #changes images into a 400px by 250px size.
-		small = cv2.cvtColor(small, cv2.COLOR_BGR2Lab) #possible color change for cvtColor
-		cv2.imwrite(string++,small)
+		small = cv2.cvtColor(small, cv2.COLOR_BGR2HSV) #possible color change for cvtColor
 		cv2.imshow('Fish filter img',small)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows
