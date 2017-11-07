9a354bf436dba3c39d43d28c7ed42df50f210e03
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-11-06 17:36:49 -0800

    ImageFilterScript.py now saves inside Results file

--- Results/ImageFilterScript.py
+++ Results/ImageFilterScript.py
@@ -8,19 +8,23 @@ from color_transfer import color_transfer
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "./Results/")
+
 filelist = glob.glob("../Samples/*.jpg",recursive=True)
 print(path)
 print(filelist)
-murky_water  = "Murkywater2.jpg"
+
+height = 400
+width = 250
+murky_water  = "Murkywater.jpg"
 water_img = cv2.imread(murky_water, 1)
-water_img = cv2.resize(water_img, (400,250))
+water_img = cv2.resize(water_img, (height,width))
 for file in filelist:
 	try:
 		img = cv2.imread(file, 1) # scrolls through all the fish images as of late.
-		small = cv2.resize(img, (400,250)) #changes images into a 400px by 250px size.
+		small = cv2.resize(img, (height,width)) #changes images into a 400px by 250px size.
 		transfer = color_transfer(water_img,small)
 		cv2.imshow('Fish filter img',transfer)
-		cv2.imwrite(img, transfer)
+		cv2.imwrite(os.path.basename(file), transfer)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows
 	except IOError as exc:
