7623481bd73415772c7be91d558073a1144ba240
Robert Campbell <robertgcampbell95@gmail.com>
2018-02-26 18:03:07 -0800

    Implement command line interface

--- Utilities/augDataSetBB.py
+++ Utilities/augDataSetBB.py
@@ -1,6 +1,20 @@
 import imagaug
 import argparse
 
+parser = argparse.ArgumentParser(description='Augments image data with the following transformations:
+crop random
+flip left right
+flip top down
+pad
+Gaussian blur
+contrast normalization
+greyscale')
+
+parser.add_argument('n', type=int, help='Number of images to generate')
+parser.add_argument('path', type=str, help='Path for folder to images')
+
+args = parser.parse_args()
+
 
 
 #
