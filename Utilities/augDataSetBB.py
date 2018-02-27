1d8b8d4f8158db290b2c0b6b91ed9a4b80016579
Robert Campbell <robertgcampbell95@gmail.com>
2018-02-26 17:56:30 -0800

    Add script to augment data while preserving bounding boxes

---
+++ Utilities/augDataSetBB.py
@@ -0,0 +1,16 @@
+import imagaug
+import argparse
+
+
+
+#
+# parse command line arguments: nOutputImages, inputDirectory, outputDirectory
+# open images in 
+# add bounding boxes 
+# create bounding boxes 
+# create transformations
+# apply transformations
+# save images
+# save bounding boxes in annotation file
+#
+# transformations: crop random, pad, flip
